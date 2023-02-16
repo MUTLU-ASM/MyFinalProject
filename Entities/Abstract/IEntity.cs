89feec5038fefce492179d5ce07eb32c68786c41
Asim Mutlu <asimmutlu6134@gmail.com>
2023-02-16 17:58:23 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden class bir vertabanı tablosudur.
+    public interface IEntity
+    {
+    }
+}
